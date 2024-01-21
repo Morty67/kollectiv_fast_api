028035a73fe4933336071dff7e0b6283595fba52
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2024-01-21 18:19:55 +0200

    Added docstring

--- app/utils/dependencies/get_session.py
+++ app/utils/dependencies/get_session.py
@@ -4,5 +4,15 @@ from app.core.database import async_session
 
 
 async def get_session() -> AsyncSession:
+    """
+    Asynchronous context manager for obtaining an SQLAlchemy AsyncSession.
+
+    This function is used to asynchronously acquire an SQLAlchemy AsyncSession
+    from the database connection pool. It should be used within
+        a `async with` statement.
+
+    Returns:
+        AsyncSession: An SQLAlchemy AsyncSession instance.
+    """
     async with async_session() as session:
         yield session
