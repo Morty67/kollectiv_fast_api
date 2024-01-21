7d39d50c2524b357a251eab0260646e66d5e86ba
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2024-01-21 18:18:29 +0200

    Added docstring

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -8,10 +8,32 @@ from app.serializers.task_serializer import (
 
 
 class TaskService:
+    """
+    Service class for handling task-related operations.
+
+    This class provides methods for interacting with tasks, including
+    creating, retrieving, updating, and deleting tasks.
+
+    Attributes:
+        task_repo (TaskRepository): The repository for handling task data.
+    """
+
     def __init__(self, task_repo: TaskRepository):
+        """
+        Initialize the TaskService with a TaskRepository instance.
+
+        Args:
+            task_repo (TaskRepository): The repository for handling task data.
+        """
         self.task_repo = task_repo
 
     async def get_all_tasks(self) -> list[TaskResponse]:
+        """
+        Get a list of all tasks.
+
+        Returns:
+            List[TaskResponse]: The list of tasks.
+        """
         tasks = await self.task_repo.get_all_tasks()
         return tasks
 
@@ -23,6 +45,19 @@ class TaskService:
         priority: str,
         user_id: int,
     ) -> TaskResponse:
+        """
+        Create a new task.
+
+        Args:
+            title (str): The title of the task.
+            description (str): The description of the task.
+            category_id (int): The ID of the category to which the task belongs.
+            priority (str): The priority of the task.
+            user_id (int): The ID of the user associated with the task.
+
+        Returns:
+            TaskResponse: The response containing the created task information.
+        """
         task_data = {
             "title": title,
             "description": description,
@@ -34,6 +69,15 @@ class TaskService:
         return self._create_task_response(task)
 
     def _create_task_response(self, task: Task) -> TaskResponse:
+        """
+        Create a TaskResponse object from a Task instance.
+
+        Args:
+            task (Task): The task instance.
+
+        Returns:
+            TaskResponse: The response containing task information.
+        """
         return TaskResponse(
             id=task.id,
             title=task.title,
@@ -44,9 +88,29 @@ class TaskService:
         )
 
     async def get_task(self, task_id: int):
+        """
+        Get task information by ID.
+
+        Args:
+            task_id (int): The ID of the task to retrieve.
+
+        Returns:
+            TaskResponse: The response containing the task information.
+        """
         return await self.task_repo.get_task_by_id(task_id)
 
     async def update_task(self, task_id: int, task_update: TaskUpdate):
+        """
+        Update an existing task.
+
+        Args:
+            task_id (int): The ID of the task to update.
+            task_update (TaskUpdate): The data for updating the task.
+
+        Returns:
+            None: If the task with the given ID doesn't exist.
+            Any: The result of the update operation.
+        """
         existing_task = await self.task_repo.get_task_by_id(task_id)
         if not existing_task:
             return None  # Task not found, return None
@@ -56,4 +120,14 @@ class TaskService:
         return updated_task
 
     async def delete_task(self, task_id: int) -> TaskDelete:
+        """
+        Delete a task by ID.
+
+        Args:
+            task_id (int): The ID of the task to delete.
+
+        Returns:
+            TaskDelete: The response containing the result of the delete
+                operation.
+        """
         return await self.task_repo.delete_task(task_id)
